caf18d2d5265d3e2b5c026700c7ad2f676f006b2
Wim Vermost <wim@pau.be>
2018-04-07 17:24:02 +0200

    Changed api response to include the chosen socket id

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,11 +4,15 @@ module.exports = function(app) {
       res.send('Thanks for visiting this API. This page has no functionality. Why are you even here.');
   })
 
-  app.get('/api/:machineId/:userId/start', function (req, res) {
-     res.json('{"message": "You started the power consumption"}');
+  app.get('/api/:socketId/:userId/start', function (req, res) {
+     res.json(
+       '{"message": "You started the power consumption", "socketId": ' + req.params.socketId + '}'
+     );
   })
 
-  app.get('/api/:machineId/:userId/stop', function (req, res) {
-     res.json('{"message": "You stopped the power consumption"}');
+  app.get('/api/:socketId/:userId/stop', function (req, res) {
+     res.json(
+       '{"message": "You stopped the power consumption", "socketId": ' + req.params.socketId + '}'
+     );
   })
 }
