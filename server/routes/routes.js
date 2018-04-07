6a8a7509a13f1f0925c4e9644dd7df5bd26703bd
Thomas Van Oekelen <thomas@pau.be>
2018-04-07 23:21:57 +0200

    create transaction via web3

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,18 +1,28 @@
 module.exports = function(app) {
 
   app.get('/', function(req, res) {
-      res.send('Thanks for visiting this API. This page has no functionality. Why are you even here.');
+    res.send('Thanks for visiting this API. This page has no functionality. Why are you even here.');
   })
 
   app.get('/api/:socketId/:userId/start', function (req, res) {
-     res.json(
-       '{"message": "You started the power consumption", "socketId": ' + req.params.socketId + '}'
-     );
+
+    MyContract.setProvider(provider)
+
+    MyContract.deployed().then(function(instance) {
+        return instance.createRecord(req.params.userId, req.params.socketId, usage,{from: '0x7E42bd617215512d021A2c6b9076BA0c080E8530'}) // wtf address
+      })
+      .catch(function(err){
+        console.log(err)
+    });
+
+    res.json(
+      '{"message": "You started the power consumption", "socketId": ' + req.params.socketId + '}'
+    );
   })
 
   app.get('/api/:socketId/:userId/stop', function (req, res) {
-     res.json(
-       '{"message": "You stopped the power consumption", "socketId": ' + req.params.socketId + '}'
-     );
+    res.json(
+      '{"message": "You stopped the power consumption", "socketId": ' + req.params.socketId + '}'
+    );
   })
 }
