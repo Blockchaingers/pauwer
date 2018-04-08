4f06ba179a591173ae020ecf2d04cfee8b504649
Wim Vermost <wim@pau.be>
2018-04-08 11:28:19 +0200

    Attempt to refactor

--- Solidity/index.js
+++ Solidity/index.js
@@ -1,6 +1,5 @@
 let Web3 = require('web3')
 let provider = new Web3.providers.HttpProvider("http://localhost:7545")
-//let web3 = new Web3(provider)
 
 let contract = require("truffle-contract");
 let json = require("./build/contracts/Recorder.json");
@@ -8,16 +7,6 @@ let json = require("./build/contracts/Recorder.json");
 let MyContract = contract(json);
 MyContract.setProvider(provider);
 
-// console.log(web3.eth.accounts);
-// var recorder = await MyContract.deployed();
-// recorder.createRecord(1,2,500);
-// MyContract.at(MyContract.address).then((v)=>{
-//   return v.createRecord(1,2,500);
-// })
-
-// MyContract.currentProvider.sendAsync = function () {
-//   return MyContract.currentProvider.send.apply(MyContract.currentProvider, arguments);
-// };
 console.log(MyContract.web3.eth.accounts[0])
 var deployed;
 MyContract.deployed().then(function(v) {
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,4 @@
-const Web3 = require('web3')
-const contract = require("truffle-contract")
-
-let provider = new Web3.providers.HttpProvider("http://localhost:7545")
-let contracts = require("../../solidity/build/contracts/Recorder.json")
-let MyContract = contract(contracts)
-
-MyContract.setProvider(provider)
+const actions = require('../controller/contractController')
 
 module.exports = function(app) {
 
@@ -13,40 +6,9 @@ module.exports = function(app) {
     res.send('Thanks for visiting this API. This page has no functionality. Why are you even here.');
   })
 
-  app.get('/api/:socketId/:userId/getState', function(req, res) {
-    MyContract.deployed().then(function(instance) {
-      return instance.getRecords(req.params.userId, req.params.socketId).then(function(result) {
-        console.log(result)
-        let lastState = result[0].map((col, i) => result.map(row => row[i]))
-        console.log(lastState)
-        res.json(lastState[lastState.length-1])
-      })
-    }).catch(function(err){
-        console.log(err)
-    });
-  })
+  app.get('/api/:socketId/:userId/getState', actions.getState);
 
-  app.get('/api/:socketId/:userId/start', function (req, res) {
-    MyContract.deployed().then(function(instance) {
-        return instance.startRecord(req.params.userId, req.params.socketId, 0,{ from: MyContract.web3.eth.accounts[0] }) // start with 0 Wh
-      })
-      .catch(function(err){
-        console.log(err)
-    });
-    res.json(
-      '{"message": "You started power consumption", "socketId": "' + req.params.socketId + '"}'
-    );
-  })
+  app.get('/api/:socketId/:userId/start', actions.start);
 
-  app.get('/api/:socketId/:userId/stop', function (req, res) {
-    MyContract.deployed().then(function(instance) {
-        return instance.stopRecord(req.params.userId, req.params.socketId, Math.random() * 100,{ from: MyContract.web3.eth.accounts[0] }) // start with 0 Wh
-      })
-      .catch(function(err){
-        console.log(err)
-    });
-    res.json(
-      '{"message": "You stopped power consumption", "socketId": "' + req.params.socketId + '"}'
-    );
-  })
+  app.get('/api/:socketId/:userId/stop', actions.stop);
 }
